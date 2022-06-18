05ca8567560c48bbb34d75f8e1f20dc3df912e4c
Aliyo254 <alinurali254@gmail.com>
2022-06-18 17:28:54 +0300

    Profle and new profile

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from .forms import NewNeighbourhoodForm
+from .forms import NewNeighbourhoodForm,NewProfileForm
 from .models import Neighbourhood,Profile,Business,Post
 # Create your views here.
 
@@ -32,3 +32,22 @@ def  neighbourhood_details(request,neighbourhood_id):
     return render(request,'details.html',{'neighbourhood':neighbourhood,'businesses':businesses,'posts':posts,})
 
 
+@login_required(login_url="/accounts/login/")
+def profile(request):
+    profiles=Profile.objects.filter(user=request.user.id)
+    neighbourhood=Neighbourhood.objects.filter(admin=request.user.id)
+    return render (request,'profile.html',{'neighbourhood':neighbourhood,'profiles':profiles,})
+@login_required(login_url="/accounts/login/")
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        profile_form = NewProfileForm(request.POST, request.FILES)
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('profile')
+
+    else:
+        profile_form = NewProfileForm()
+    return render(request, 'new_profile.html', {"profile_form": profile_form,})
