9b3018c86dd9bd38e95a0061efe6a0abad251f3c
Aliyo254 <alinurali254@gmail.com>
2022-06-18 13:39:17 +0300

    Profile  model

--- neighbour/models.py
+++ neighbour/models.py
@@ -8,3 +8,11 @@ class Neighbourhood(models.Model):
     population=models.IntegerField()
     admin=models.ForeignKey(User,on_delete=models.CASCADE)
 
+
+
+
+class Profile(models.Model):
+    profile_pic=models.ImageField(upload_to='profile_photos/')
+    bio=models.CharField(max_length=300)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighborhood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
