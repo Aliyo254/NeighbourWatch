216827acae58f7017c89d3256a125aa5943c7691
Aliyo254 <alinurali254@gmail.com>
2022-06-18 13:38:49 +0300

    Neighbourhood model

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Neighbourhood(models.Model):
+    name=models.CharField(max_length=60)
+    location=models.CharField(max_length=60)
+    population=models.IntegerField()
+    admin=models.ForeignKey(User,on_delete=models.CASCADE)
+
