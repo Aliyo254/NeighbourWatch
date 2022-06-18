c65f194c465b0cb74a1cc642b65a23f925d183a9
Aliyo254 <alinurali254@gmail.com>
2022-06-18 13:46:17 +0300

    Neighbourhood model methods

--- neighbour/models.py
+++ neighbour/models.py
@@ -8,6 +8,32 @@ class Neighbourhood(models.Model):
     population=models.IntegerField()
     admin=models.ForeignKey(User,on_delete=models.CASCADE)
 
+    def create_neigborhood(self):
+        self.save()
+
+    def delete_neigborhood(self):
+        self.delete()
+    @classmethod
+    def find_neigborhood(cls,neighborhood_id):
+        neighborhood=cls.objects.filter(id=neighborhood_id)
+        return neighborhood
+    @classmethod
+    def update_neighborhood(cls,neighborhood_id):
+        neighborhood=cls.objects.filter(id=neighborhood_id)
+        neighborhood.name=Value
+        neighborhood.save()
+        return neighborhood
+    @classmethod
+    def update_neighborhood(cls,neighborhood_id):
+        neighborhood=cls.objects.filter(id=neighborhood_id)
+        neighborhood.occupants=Value
+        neighborhood.save()
+        return neighborhood
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        neighborhood=cls.objects.filter(name__icontains=search_term)
+        return neighborhood
 
 
 
